8167c00a2b17f813d6c77f70dc4bb5d7b702ef76
Kyeore Heo <91kyoheo@gmail.com>
2021-02-16 18:56:59 -0500

    added awseom functions

--- main.cpp
+++ main.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 
+void foo() {
+	cout << "I love C++" << endl;
+}
+
+void foo2() {
+	cout << "I love C++ so much" << endl;
+}
+
+bool loveC() {
+	
+}
+
 int main() {
 	for(int i = 0; i < 10; i++) {
 		cout << i << endl;
