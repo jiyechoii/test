783046c22db2d7ed844e3b60eea2058bbc7cabe3
jiyechoii <jcho2302@gmail.com>
2021-02-16 19:01:56 -0500

    firstcommit

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 
 using namespace std;
+//find minimum value from vector of int
 
 int main() {
+	vector<int> numbers = {1, 50, 4, -1};
 	for(int i = 0; i < 10; i++) {
 		cout << i << endl;
 	}
